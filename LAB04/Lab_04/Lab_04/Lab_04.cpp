eef6ef4d0dc6aabe57ea57ba0411df7df65ec0fa
Lift2007 <6811130008@mut.ac.th>
2025-12-09 16:51:07 +0700

    อัพงานLAB4

---
+++ LAB04/Lab_04/Lab_04/Lab_04.cpp
@@ -0,0 +1,17 @@
+// Lab_04.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  
+    for (int i = 1; i <= 12; i++) {
+
+        
+        cout << "8 x" << i << " = " << 8 * i << endl;
+    }
+            return 0;
+        }
+    
---
+++ LAB04/Lab_14/Lab_14/Lab_14.cpp
@@ -0,0 +1,14 @@
+// Lab_14.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 0;i <= 20;i++) {
+		if (i % 2==0)cout << i << endl;
+	}
+
+		
+}
