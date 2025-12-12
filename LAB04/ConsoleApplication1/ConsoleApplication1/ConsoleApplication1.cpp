8c4ae0351a1501b3781a28bff819904a0be85841
Lift2007 <6811130008@mut.ac.th>
2025-12-12 19:16:57 +0700

    wroksogood

--- LAB04/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ LAB04/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,22 +8,14 @@ using namespace std;
 int main()
 
 {
-    int n1 = 0 ,n2 = 1;
-    for (int i = 1; i <= 10; i++) {
-        cout << i << " " << endl;
-}   
-    while (n1 < 10) {
-        n1++; 
-        cout << n1 << endl;
-    }
+    int sumi = 0;
+    for (int i = 0; i <= 100; i++)
+    {
+        sumi += i;
+    } cout << sumi;
 
-    do {
-        cout << n2 << endl;
-        n2++;
-    } while (n2 <= 10);
-
-    return 0;
     }
+    
     
 
 
